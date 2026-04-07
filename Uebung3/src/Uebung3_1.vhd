13c227cf95e166540666e152f3254d3f7a1b667f
Leander Kieweg <leander.kieweg@gmail.com>
2026-04-08 00:10:43 +0200

    Add VHDL architecture and stimulus process for Uebung3_1; create ModelSim configuration file

--- Uebung3/src/Uebung3_1.vhd
+++ Uebung3/src/Uebung3_1.vhd
@@ -1,6 +1,29 @@
 	-- signal Q:      bit := '1';
 	-- signal R,S,Qn: bit := '0';   
 
+entity uebung3 is
+end entity uebung3;
+
+architecture behav of uebung3 is
+
+  signal Q : bit := '1';
+  signal Qn : bit := '0';
+  signal r, s : bit := '0';
+
+begin
+
+  Nor2: process is
+  begin
+    Qn <= Q nor S after 0 ns;
+    wait on Q, S;
+  end process Nor2;
+
+  Nor1: process is
+  begin
+    Q <= Qn nor R after 0 ns;
+    wait on Qn, R;
+  end process Nor1;
+
 Stimul: process
    begin
    
@@ -14,3 +37,5 @@ Stimul: process
 		 '0' after 600 ns;   
     wait;
 end process;
+
+end architecture behav;
