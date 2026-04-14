66eddc680356b4d4189c47d96ece71e6a9e97a17
Leander Kieweg <leander.kieweg@gmail.com>
2026-04-14 12:41:38 +0200

    Add VHDL entity and architecture for signal assignment demonstration
    - Created a new VHDL file `Uebung4_2.vhd` containing an entity named `Assign`.
    - Implemented an architecture `Bhv` with a process that demonstrates various signal assignments and variable manipulations.
    - Included comments indicating points for signal and driver observations.

---
+++ Uebung4/src/Uebung4_2.vhd
@@ -0,0 +1,35 @@
+entity Assign is
+end entity Assign;
+
+architecture Bhv of Assign is 
+signal A, B, C, D: real := 1.9;
+begin
+  OneAndOnly: process is
+    variable Z: real := 39.0;
+  begin
+    A <= 3.5 after 23 ns,
+         7.8 after 31 ns,
+         5.8 after 56 ns;     
+    B <= transport 4.5 after 22 ns,
+                   4.8 after 56 ns,
+                   2.3 after 177 ns;
+    C <= reject 34 ns inertial 4.2 after 41 ns;
+    -- a) Bitte jetzt A,B,C,D und ihre driver und Z aufzeichnen
+  
+    D <= transport B+1.1 after 10 ns;
+    B <= transport 5.3 after 13 ns;
+    Z := Z-1.4;
+    -- b) Bitte jetzt D,B,Z sowie driver von D und B aufzeichnen.
+  
+    A <= 78.1 after 163 ns;
+    Z:= B;
+    -- c) Bitte jetzt A, Z und den driver von A aufzeichnen
+  
+    D <= 7.9 after 71 ns;
+    C <= reject 63 ns inertial 2.3 after 89 ns;
+    -- d) Bitte jetzt C, D und zugehoerige driver aufzeichnen
+  
+    wait;
+  end process OneAndOnly;
+
+end architecture Bhv;
