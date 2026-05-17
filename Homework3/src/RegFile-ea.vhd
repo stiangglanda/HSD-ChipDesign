d2341a18bb4ce9a7b3c777d1fdce9d2e6479d657
Leander Kieweg <leander.kieweg@gmail.com>
2026-05-17 17:08:20 +0200

    Enhance RegFile functionality with improved read/write processes; update library references in vhdl_ls.toml

--- Homework3/src/RegFile-ea.vhd
+++ Homework3/src/RegFile-ea.vhd
@@ -28,16 +28,27 @@ entity RegFile is
 end entity RegFile;
 
 architecture Bhv of RegFile is
+    type reg_array is array (cMaxRegAdr-1 downto 0) of aData;
+    signal vReg_array : reg_array;
 begin
     ReadReg: process
+        variable vIndex1 : natural;
+        variable vIndex2 : natural;
     begin
+        vIndex1 := toNat(iA1);
+        vIndex2 := toNat(iA2);
+        oRD1 <= iR15 when vIndex1 = cMaxRegAdr else vReg_array(vIndex1);
+        oRD2 <= iR15 when vIndex2 = cMaxRegAdr else vReg_array(vIndex2);
 
+        wait on iA1, iA2;
     end process;
 
     WriteReg: process
+        variable vIndex : natural;
     begin
-        if iClk'event and iClk = '1' then
-
+        if iClk'event and iClk = '1' and iWE3 = '1' then
+            vIndex := toNat(iA3);
+            vReg_array(vIndex) <= iWD3;
         end if;
         wait on iClk;
     end process;
