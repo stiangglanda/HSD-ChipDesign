2a8f1086d08aec93eb2f973ffa52fe19d5399e85
Leander Kieweg <leander.kieweg@gmail.com>
2026-05-17 13:04:05 +0200

    Implement Program Counter functionality with reset and clock handling; update library references in vhdl_ls.toml

--- Homework3/src/PC-ea.vhd
+++ Homework3/src/PC-ea.vhd
@@ -17,11 +17,24 @@ use work.SingleCycleCPUPack.all;
 entity PC is
    port (
 		-- Hier die Ports ergänzen.
-        iClk : in  std_ulogic);
+        iClk : in  std_ulogic;
+        inRST : in  std_ulogic;
+        iPCnext : in aPCvalue;
+        oPCcurr : out aPCvalue);
 end entity PC;
 
 architecture Bhv of PC is
    begin
    -- siehe flankengesteuertes DFF
+      process -- (iCLK, inRST) sensitivity list
+    begin
+        if inRST = '0' then
+            -- wait for 10 ns; sensitivity liste nicht erlaubt
+            oPCcurr <= (others => '0')  after 3 ns;
+        elsif iCLK'event and iCLK = '1' then
+            oPCcurr <= iPCnext after 4 ns;
+        end if;
+        wait on iCLK, inRST;
+    end process;
 end architecture Bhv;
 
