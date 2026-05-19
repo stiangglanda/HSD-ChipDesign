82ab7d41743d4e99c7e2a65dcec807bca4f7a484
Leander Kieweg <leander.kieweg@gmail.com>
2026-05-19 12:29:12 +0200

    Refactor clkTree architecture in U61.vhd and add inertial behavior in U61_inertial.vhd

--- Uebung6/src/U61_inertial.vhd
+++ Uebung6/src/U61_inertial.vhd
@@ -4,10 +4,10 @@ use ieee.std_logic_1164.all;
 entity clkTree is
 end entity;
 
-architecture bhv_transport of clkTree is
+architecture bhv_inertial of clkTree is
     signal clk : std_ulogic := '0';
     signal y1, y2, y3, y4 : std_ulogic;
-    constant cClkPhase : time := 2 ns;
+    constant cClkPhase : time := 10 ns;
     constant cBufferDelay : time := 10 ns;
 begin
     CG: process is
@@ -18,18 +18,18 @@ begin
 
     B1: process is
     begin
-        y1 <= transport clk after cBufferDelay;
+        y1 <= reject cBufferDelay inertial clk after cBufferDelay;
         wait on clk;
     end process;
 
     B2: process (y1) is    --sensitivitylist
     begin
-        y2 <= transport y1 after cBufferDelay;
+        y2 <= y1 after cBufferDelay; -- inertial implicit
         -- wait on y1;
     end process;
 
-    B3: y3 <= transport y2 after cBufferDelay; -- concurrent signal assignment
+    B3: y3 <= y2 after cBufferDelay; -- concurrent signal assignment
 
-    B13: y4 <= transport clk after 3*cBufferDelay;
+    B13: y4 <= reject cBufferDelay inertial clk after 3*cBufferDelay;
 
-end architecture bhv_transport;
+end architecture bhv_inertial;
