43ff665fa2dbafd432bac9e7a2bfb60c4e4c67c5
Leander Kieweg <leander.kieweg@gmail.com>
2026-05-26 12:56:42 +0200

    Update library references in vhdl_ls.toml and add Uebung3 entity with architecture in Uebung3_1.vhd

---
+++ Uebung5/src/Uebung3_1.vhd
@@ -0,0 +1,42 @@
+	-- signal Q:      bit := '1';
+	-- signal R,S,Qn: bit := '0';   
+
+entity uebung3 is
+end entity uebung3;
+
+architecture behav of uebung3 is
+
+  signal Q : bit := '1';
+  signal Qn : bit := '0';
+  signal r, s : bit := '0';
+  constant cTpd : time := 10 ns;
+
+begin
+
+  Nor2: process is -- s = 1 R = 0
+  begin
+    Qn <= Q nor S after cTpd; -- 20 ns Qn= 0 
+    wait on Q, S;
+  end process Nor2;
+
+  Nor1: process is
+  begin
+    Q <= Qn nor R after cTpd; -- 20 ns Q= 1
+    wait on Qn, R;
+  end process Nor1;
+
+Stimul: process
+   begin
+   
+    S <= '1' after 300 ns,
+		 '0' after 400 ns,
+		 '1' after 500 ns,
+		 '0' after 600 ns;
+    R <= '1' after 100 ns,
+		 '0' after 200 ns,
+		 '1' after 500 ns,
+		 '0' after 600 ns;   
+    wait;
+end process;
+
+end architecture behav;
